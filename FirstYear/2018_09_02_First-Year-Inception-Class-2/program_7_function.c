7d4ad584017b4be6eb57bb7446ba953bb12a2eb3
packetChor <cs154061@mnnit.ac.in>
2018-09-02 16:06:25 +0530

    added first year inception class 2 material

---
+++ FirstYear/2018_09_02_First-Year-Inception-Class-2/program_7_function.c
@@ -0,0 +1,27 @@
+// @author: Akshay Sharma(akshay31057)
+
+#include<stdio.h>
+
+void add(int a, int b)
+{
+	int sum = a + b;
+	//return sum;
+	printf("Sum  = %d\n",sum );
+}
+
+//return_type function_name(datatype argument_1, datatype argument_2)
+//{
+ 	/*---code statememts----
+ 	---code statememts----
+ 	---code statememts----*/
+//}
+
+int main()
+{
+	int a = 2, b = 5;
+	/*int sum = */add(a, b);
+//	printf("Sum = %d", sum);
+	printf("\\\\n");
+
+	return 0;
+}
